5709f4929230496abb3f9d5a313f2635d2e58e1e
Fabit30 <60932721+Fabit30@users.noreply.github.com>
2020-07-24 20:49:40 -0500

    Clase Aplicacion

--- sample1-git/src/main/java/ec/edu/epn/git/Aplicacion.java
+++ sample1-git/src/main/java/ec/edu/epn/git/Aplicacion.java
@@ -4,7 +4,8 @@ public class Aplicacion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//editor e=new editor();
+		editor e=new editor();
+		System.out.println("Hola Mundo");
 
 	}
 
